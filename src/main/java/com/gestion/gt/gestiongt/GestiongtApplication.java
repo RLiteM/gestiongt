90f25f1834f15af4ad28082016efef67190fe2b3
RLiteM <mvicentem6@miumg.edu.gt>
2025-08-05 21:16:46 -0600

    Inicializando proyecto

---
+++ src/main/java/com/gestion/gt/gestiongt/GestiongtApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.gt.gestiongt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiongtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiongtApplication.class, args);
+	}
+
+}
